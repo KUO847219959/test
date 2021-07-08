8270a16ff1b9169e8fa941b965e639c31a4fd49f
Kuo <847219959@qq.com>
2021-07-08 19:37:45 +0800

    origin_note

---
+++ VINS-Mono/feature_tracker/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/lab-307/slam_ws/src/VINS-Mono/feature_tracker
+# Build directory: /home/lab-307/slam_ws/src/VINS-Mono/feature_tracker/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ VINS-Mono/feature_tracker/cmake-build-debug/catkin_generated/ordered_paths.cmake
@@ -0,0 +1,1 @@
+set(ORDERED_PATHS "/home/lab-307/slam_ws/devel/lib;/opt/ros/kinetic/lib")
